eb5b4c19253b093bd81b9f7e4d65372df240d34c
Tinnaphop <tinnaphop.t@st.econ.tu.ac.th>
2024-06-01 12:16:39 +0700

    take assignment

--- ex-1.js
+++ ex-1.js
@@ -1,8 +1,8 @@
 let words = ["hello", "world"];
 
 function getWordLengths(words) {
-  // Start coding here
-}
+  return words.map(word => word.length);
+};
 
 const result = getWordLengths(words);
 
